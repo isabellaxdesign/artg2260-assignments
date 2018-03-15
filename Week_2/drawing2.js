438ac063eff9c09f9b6de775665d0d86bb66933d
Isa By Design <isabellabfdiaz@gmail.com>
2018-03-15 19:46:27 -0400

    tile slight change

--- Week_2/drawing2.js
+++ Week_2/drawing2.js
@@ -2,7 +2,7 @@ function setup() {
 	createCanvas(400, 400);
 	background(50)
 	
-	for (var i = 0; i < 400; i = i+5){
+	for (var i = 0; i < 400; i = i+10){
 		rect(i, i+2, 400,400 );
 		fill(i*2,i,i+100)
 	
