b39a9303bbf8a04404a9a4ee1e08a05b9dc5333d
Isa By Design <isabellabfdiaz@gmail.com>
2018-03-29 15:41:02 -0400

    week 9 assignment

--- Excersises/Week_9/sketch.js
+++ Excersises/Week_9/sketch.js
@@ -1,5 +1,4 @@
-
-let song;
+let song,slider,panSlider,rateSlider;
 
 function preload(){
   song = loadSound("assets/simplicity.mp3");
@@ -7,4 +6,18 @@ function preload(){
 
 function setup() {
   song.play();
+  slider = createSlider(0, 1, 0.5, 0.01);
+  
+  song.pan();
+  panSlider=createSlider(-1,1,0,0.1);
+
+  song.rate();
+  rateSlider=(0.5,2,1,0.1);
 }
+
+function draw(){
+  song.setVolume(slider.value());
+  song.pan(panSlider.value());
+  song.rate(rateSlider.value());
+}
+
---
+++ Week_9/drawing9.js
@@ -0,0 +1,32 @@
+var song;
+var button;
+var amp:
+var vol;
+
+function toggleSong(){
+  if(song.isPlaying()){
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+function preload(){
+  song= loadSound("assets/intro.mp3");
+
+}
+
+function setup(){
+  createCanvas(200,200);
+  button=createButton('toggle');
+  button.mousePressed(toggleSong);
+  song.play();
+  amp = new p5.Amplitude();
+}
+
+function draw(){
+  background(0);
+  var vol =amp.getLevel();
+  ellipse(100,100,200,vol *200);
+  fill(255,240,245);
+}
